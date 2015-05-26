0623349d9b91cf5fefd887b9aec836878c3d952a
Fabrice Colas <fcolas@ubivar.com>
2015-05-26 15:56:27 +0200

    Bump version to 1.0

--- lib/Ubivar.php
+++ lib/Ubivar.php
@@ -16,7 +16,7 @@ class Ubivar
     // @var boolean Defaults to true.
     public static $verifySslCerts = true;
 
-    const VERSION = '0.1.3-beta';
+    const VERSION = '1.0.0-beta';
 
     /**
      * @return string The API key used for requests.
