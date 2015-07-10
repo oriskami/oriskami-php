58d3680a4c89789551874a1985126762faa8db76
Fabrice Colas <fcolas@ubivar.com>
2015-07-10 17:32:17 +0200

    Fix left over curly-bracket when adding pluralization support of addr

--- lib/ApiResource.php
+++ lib/ApiResource.php
@@ -58,7 +58,7 @@ abstract class ApiResource extends Object
     {
         $base               = static::className();
         $noPluralResources  = array("me","login","logout","routing");
-        if (in_array($base, $noPluralResources)) {
+        if (in_array($base, $noPluralResources)) 
             return "/${base}";
         else if ($base === 'address')
             return "/${base}es";
