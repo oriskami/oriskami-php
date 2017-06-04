eb5d864f7ed993a7d5996a28ee7ef96bb4de2193
Fabrice Colas <fcolas@ubivar.com>
2017-06-04 17:58:47 +0200

    add resources to init file

--- init.php
+++ init.php
@@ -24,5 +24,22 @@ require(dirname(__FILE__) . '/lib/SingletonApiResource.php');
 require(dirname(__FILE__) . '/lib/AttachedObject.php');
 
 // Ubivar API Resources
-require(dirname(__FILE__) . '/lib/Me.php');
 require(dirname(__FILE__) . '/lib/Event.php');
+require(dirname(__FILE__) . '/lib/EventNotification.php');
+require(dirname(__FILE__) . '/lib/EventLastId.php');
+require(dirname(__FILE__) . '/lib/EventLabel.php');
+require(dirname(__FILE__) . '/lib/EventQueue.php');
+require(dirname(__FILE__) . '/lib/EventReview.php');
+
+require(dirname(__FILE__) . '/lib/FilterWhitelist.php');
+require(dirname(__FILE__) . '/lib/FilterBlacklist.php');
+require(dirname(__FILE__) . '/lib/FilterRulesCustom.php');
+require(dirname(__FILE__) . '/lib/FilterRulesBase.php');
+require(dirname(__FILE__) . '/lib/FilterRulesAI.php');
+require(dirname(__FILE__) . '/lib/FilterScoringsDedicated.php');
+
+require(dirname(__FILE__) . '/lib/NotifierEmail.php');
+require(dirname(__FILE__) . '/lib/NotifierSms.php');
+require(dirname(__FILE__) . '/lib/NotifierSlack.php');
+require(dirname(__FILE__) . '/lib/NotifierWebhook.php');
+require(dirname(__FILE__) . '/lib/NotifierECommerce.php');
